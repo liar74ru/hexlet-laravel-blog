a8249278427fbdd8cdae48dc42dfb796d534e383
Vladimir Kipriyanov <liar2707@gmail.com>
2025-11-14 21:04:07 +0500

    Урок 4

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+@section('header')
+    <h2>Header About</h2>
+@endsection
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
---
+++ resources/views/articles.blade.php
@@ -0,0 +1,9 @@
+<!-- BEGIN (write your solution here) -->
+@extends('layouts.app')
+
+@section('header', 'Статьи')
+
+@section('conten')
+    <p>Тут будут статьи</p>
+@endsection
+<!-- END -->
